6cd517d306976f12f2ab94acb75f636af753d0ae
Tim-ats-d <tim.arnouts@protonmail.com>
2023-02-25 14:18:18 +0100

    Everything in one file.

--- qotd-server/unikernel.ml
+++ qotd-server/unikernel.ml
@@ -1,6 +1,94 @@
-let src = Logs.Src.create "guillemet"
+module Quote : sig
+  type t = private string
 
-module Log = (val Logs.src_log src)
+  val make :
+    ?non_ascii:[ `Enable | `Ignore | `Raise ] ->
+    ?size_exceed:[ `Raise | `Truncate ] ->
+    string ->
+    t
+
+  val to_string : t -> string
+end = struct
+  type t = string
+
+  let print strategy s =
+    let buf = Buffer.create 512 in
+    String.iter
+      (fun chr ->
+        match (chr, strategy) with
+        | c, `Enable -> Buffer.add_char buf c
+        | ((' ' .. '~' | '\r' | '\n') as c), _ -> Buffer.add_char buf c
+        | _, `Ignore -> ()
+        | _, `Raise -> invalid_arg "print")
+      s;
+    Buffer.contents buf
+
+  let make ?(non_ascii = `Ignore) ?(size_exceed = `Truncate) s =
+    let len = String.length s in
+    let quote =
+      match size_exceed with
+      | `Raise when len > 512 -> invalid_arg "Quote.make"
+      | `Truncate when len > 512 -> String.sub s 0 512
+      | `Raise | `Truncate -> s
+    in
+    print non_ascii quote
+
+  let to_string s = s
+end
+
+module QotdServerMake (S : Tcpip.Stack.V4V6) : sig
+  val run :
+    ?on_error:([ `TCP of S.TCP.write_error | `UDP of S.UDP.error ] -> unit) ->
+    ?port:int ->
+    S.t ->
+    (unit -> Quote.t Lwt.t) ->
+    unit Lwt.t
+end = struct
+  open Lwt.Infix
+  open Lwt.Syntax
+
+  let src = Logs.Src.create "guillemet-server"
+
+  module Log = (val Logs.src_log src)
+
+  let served quote =
+    let len = String.length quote in
+    let quote_view =
+      if len < 30 then quote
+      else String.sub quote 0 30 |> Printf.sprintf "%s [...]"
+    in
+    Log.info (fun log -> log "Served %S" quote_view)
+
+  let warn_err = function
+    | `TCP err ->
+        Log.warn (fun log -> log "TCP error: %a" S.TCP.pp_write_error err)
+    | `UDP err -> Log.warn (fun log -> log "UDP error: %a" S.UDP.pp_error err)
+
+  let listen_tcp ~on_error ~port stack quote_of_the_day =
+    S.TCP.listen stack ~port (fun flow ->
+        let* qotd = quote_of_the_day () >|= Quote.to_string in
+        let* () =
+          Cstruct.of_string qotd |> S.TCP.write flow >|= function
+          | Ok () -> served qotd
+          | Error err -> on_error (`TCP err)
+        in
+        S.TCP.close flow)
+
+  let listen_udp ~on_error ~port stack quote_of_the_day =
+    S.UDP.listen stack ~port (fun ~src ~dst:_ ~src_port _ ->
+        let* qotd = quote_of_the_day () in
+        let qotd_str = Quote.to_string qotd in
+        Cstruct.of_string qotd_str
+        |> S.UDP.write ~src_port:17 ~dst:src ~dst_port:src_port stack
+        >|= function
+        | Ok () -> served qotd_str
+        | Error err -> on_error (`UDP err))
+
+  let run ?(on_error = warn_err) ?(port = 17) stack qotd =
+    listen_tcp ~on_error ~port (S.tcp stack) qotd;
+    listen_udp ~on_error ~port (S.udp stack) qotd;
+    S.listen stack
+end
 
 module Main (_ : sig end)
 (Pclock : Mirage_clock.PCLOCK)
@@ -8,9 +96,13 @@ module Main (_ : sig end)
 (Time : Mirage_time.S) =
 struct
   module Store = Git_kv.Make (Pclock)
-  module QotdServer = Server.Make (Stack)
+  module QotdServer = QotdServerMake (Stack)
   open Lwt.Syntax
   open Lwt.Infix
+
+  let src = Logs.Src.create "guillemet"
+
+  module Log = (val Logs.src_log src)
 
   type t = { mutable quotes : Store.key Queue.t; mutable today : Ptime.date }
 
@@ -112,8 +204,7 @@ struct
     let context = create () in
     let* () = fill_queue store context in
     let listen =
-      QotdServer.run ?port:(Key_gen.port ()) stack
-        (serve_qotd store context)
+      QotdServer.run ?port:(Key_gen.port ()) stack (serve_qotd store context)
     in
     Lwt.both listen (sync store context)
 end
--- server/quote.mli
+++
@@ -1,9 +0,0 @@
-type t = private string
-
-val make :
-  ?non_ascii:[ `Enable | `Ignore | `Raise ] ->
-  ?size_exceed:[ `Raise | `Truncate ] ->
-  string ->
-  t
-
-val to_string : t -> string
